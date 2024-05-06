fb79fa57b713d9caac3cae0999bd5d241c6e7ef2
merabza <merabza@gmail.com>
2024-05-06 20:42:20 +0400

    202405062042

---
+++ RazorPagesTools/RazorPagesInstaller.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using WebInstallers;
+
+namespace RazorPagesTools;
+
+// ReSharper disable once UnusedType.Global
+public sealed class RazorPagesInstaller : IInstaller
+{
+    public int InstallPriority => 14;
+    public int ServiceUsePriority => 136;
+
+    public void InstallServices(WebApplicationBuilder builder, string[] args)
+    {
+        //Console.WriteLine("StaticFilesInstaller.InstallServices Started");
+
+        builder.Services.AddRazorPages();
+
+        //Console.WriteLine("StaticFilesInstaller.InstallServices Finished");
+    }
+
+    public void UseServices(WebApplication app)
+    {
+        //Console.WriteLine("StaticFilesInstaller.UseMiddleware Started");
+
+        app.UseRouting();
+
+        //app.UseAuthorization();
+
+        app.MapRazorPages();
+
+        //Console.WriteLine("StaticFilesInstaller.UseMiddleware Finished");
+    }
+}
