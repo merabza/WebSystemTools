0304545797bd83b7aefbeee867882bd56b72df36
merabza <merabza@gmail.com>
2024-05-17 19:01:11 +0400

    202405171900

---
+++ HttpClientInstaller/ClientInstaller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using WebInstallers;
+
+namespace HttpClientInstaller;
+
+// ReSharper disable once UnusedType.Global
+public sealed class ClientInstaller : IInstaller
+{
+    public int InstallPriority => 10;
+    public int ServiceUsePriority => 10;
+
+    public void InstallServices(WebApplicationBuilder builder, string[] args, Dictionary<string, string> parameters)
+    {
+        //Console.WriteLine("ConfigurationEncryptInstaller.InstallServices Started");
+
+        builder.Services.AddHttpClient();
+        
+        //Console.WriteLine("ConfigurationEncryptInstaller.InstallServices Finished");
+    }
+
+    public void UseServices(WebApplication app)
+    {
+    }
+}
