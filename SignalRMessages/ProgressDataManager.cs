bac5adf9a6f14d2c2d67b0cc6fc2e19665a4f07d
merabza <merabza@gmail.com>
2024-05-29 14:48:10 +0400

    202405291447

--- SignalRMessages/ProgressDataManager.cs
+++ SignalRMessages/ProgressDataManager.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 using ReCounterDom;
+using SignalRContracts;
 using SignalRContracts.Models;
 
 namespace SignalRMessages;
@@ -13,7 +14,7 @@ public class ProgressDataManager : IProgressDataManager, IDisposable, IAsyncDisp
 {
     private static readonly object SyncRoot = new();
     private readonly HashSet<string> _connectedIds = [];
-    private readonly IHubContext<ReCounterMessagesHub> _hub;
+    private readonly IHubContext<ReCounterMessagesHub, IProgressDataMessenger> _hub;
     private readonly ILogger<ProgressDataManager> _logger;
     private int _currentChangeId;
     private ProgressData? _currentData;
@@ -24,7 +25,7 @@ public class ProgressDataManager : IProgressDataManager, IDisposable, IAsyncDisp
     private bool _timerStarted;
 
     // ReSharper disable once ConvertToPrimaryConstructor
-    public ProgressDataManager(IHubContext<ReCounterMessagesHub> hub, ILogger<ProgressDataManager> logger)
+    public ProgressDataManager(IHubContext<ReCounterMessagesHub, IProgressDataMessenger> hub, ILogger<ProgressDataManager> logger)
     {
         _hub = hub;
         _logger = logger;
@@ -61,7 +62,7 @@ public class ProgressDataManager : IProgressDataManager, IDisposable, IAsyncDisp
     }
 
 
-    public void SetProgressData(string name, string message, bool instantly = false)
+    public async Task SetProgressData(string name, string message, bool instantly, CancellationToken cancellationToken)
     {
         CheckTimer();
         lock (SyncRoot)
@@ -74,10 +75,10 @@ public class ProgressDataManager : IProgressDataManager, IDisposable, IAsyncDisp
         }
 
         if (instantly && _connectedIds.Count > 0)
-            SendData(_lastChangesData);
+            await SendData(_lastChangesData, cancellationToken);
     }
 
-    public void SetProgressData(string name, bool value, bool instantly = true)
+    public async Task SetProgressData(string name, bool value, bool instantly, CancellationToken cancellationToken)
     {
         lock (SyncRoot)
         {
@@ -89,10 +90,10 @@ public class ProgressDataManager : IProgressDataManager, IDisposable, IAsyncDisp
         }
 
         if (instantly && _connectedIds.Count > 0)
-            SendData(_lastChangesData);
+            await SendData(_lastChangesData, cancellationToken);
     }
 
-    public void SetProgressData(string name, int value, bool instantly = false)
+    public async Task SetProgressData(string name, int value, bool instantly, CancellationToken cancellationToken)
     {
         CheckTimer();
         lock (SyncRoot)
@@ -105,7 +106,7 @@ public class ProgressDataManager : IProgressDataManager, IDisposable, IAsyncDisp
         }
 
         if (instantly && _connectedIds.Count > 0)
-            SendData(_lastChangesData);
+            await SendData(_lastChangesData, cancellationToken);
     }
 
     private void StartTimer()
@@ -121,20 +122,22 @@ public class ProgressDataManager : IProgressDataManager, IDisposable, IAsyncDisp
         if (_sentChangeId == _currentChangeId)
             return;
         if (_lastChangesData is not null)
-            SendData(_lastChangesData);
+            SendData(_lastChangesData, CancellationToken.None).Wait();
     }
 
-    private void SendData(ProgressData progressData)
+    private async Task SendData(ProgressData progressData, CancellationToken cancellationToken)
     {
         _sentChangeId = _currentChangeId;
-        SendNotificationAsync(progressData).Wait();
+        //SendNotificationAsync(progressData).Wait();
+        await _hub.Clients.All.SendProgressData(progressData, cancellationToken);
+
         _lastChangesData = null;
     }
 
-    private Task SendNotificationAsync(object objectToSend)
-    {
-        return _hub.Clients.All.SendAsync("sendtoall", objectToSend);
-    }
+    //private Task SendNotificationAsync(object objectToSend)
+    //{
+    //    return _hub.Clients.All.SendAsync("sendtoall", objectToSend);
+    //}
 
     private void CheckTimer()
     {
