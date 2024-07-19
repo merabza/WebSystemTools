25d64194c30090459f97f2e9feccf8b5e048e15b
merabza <merabza@gmail.com>
2024-07-19 21:17:37 +0400

    202407192116

--- BackgroundTasksTools/AssemblyReference.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-namespace BackgroundTasksTools;
-
-public static class AssemblyReference
-{
-    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
-}
