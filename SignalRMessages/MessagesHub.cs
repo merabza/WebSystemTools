f1dd4af8b4f6ca018eee159b5b99b65810557a3e
merabza <merabza@gmail.com>
2024-10-25 01:35:14 +0400

    202410250134

---
+++ SignalRMessages/MessagesHub.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using StringMessagesApiContracts;
+using SystemToolsShared;
+
+namespace SignalRMessages;
+
+public class MessagesHub : Hub<IMessenger>
+{
+    private readonly IMessagesDataManager _messagesDataManager;
+
+    // ReSharper disable once ConvertToPrimaryConstructor
+    public MessagesHub(IMessagesDataManager messagesDataManager)
+    {
+        _messagesDataManager = messagesDataManager;
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        //_userCount ++;
+        if (Context.UserIdentifier is not null)
+            _messagesDataManager.UserConnected(Context.ConnectionId, Context.UserIdentifier);
+        return base.OnConnectedAsync();
+    }
+
+    public override Task OnDisconnectedAsync(Exception? exception)
+    {
+        //_userCount --;
+        if (Context.UserIdentifier is not null)
+            _messagesDataManager.UserDisconnected(Context.ConnectionId, Context.UserIdentifier);
+        return base.OnDisconnectedAsync(exception);
+    }
+}
