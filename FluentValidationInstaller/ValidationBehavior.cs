9b5ec7536bbe3c252892801286bdca30742213e5
merabza <merabza@gmail.com>
2025-03-20 22:30:05 +0400

    202503202226

--- FluentValidationInstaller/ValidationBehavior.cs
+++ FluentValidationInstaller/ValidationBehavior.cs
@@ -4,7 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using FluentValidation;
 using MediatR;
-using MessagingAbstractions;
+using MediatRMessagingAbstractions;
 using OneOf;
 using SystemToolsShared.Errors;
 
