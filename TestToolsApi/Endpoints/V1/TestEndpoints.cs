759ed8131b881b0646c7394c1fccacedd2ac899a
merabza <merabza@gmail.com>
2025-10-02 20:47:38 +0400

    202510022047

--- TestToolsApi/Endpoints/V1/TestEndpoints.cs
+++ TestToolsApi/Endpoints/V1/TestEndpoints.cs
@@ -58,34 +58,34 @@ public sealed class TestEndpoints : IInstaller
     }
 
     // GET api/v1/test/getip
-    private static Ok<string> GetIp(ILogger<TestEndpoints> logger, HttpRequest request)
+    private static IResult GetIp(ILogger<TestEndpoints> logger, HttpRequest request)
     {
         var ret = $"{request.HttpContext.Connection.RemoteIpAddress} {Assembly.GetEntryAssembly()?.GetName().Version}";
         logger.LogInformation("Test from {ret}", ret);
-        return TypedResults.Ok(ret);
+        return Results.Text(ret, "text/plain");
     }
 
     // GET api/v1/test/getversion
-    private static Ok<string> Version(ILogger<TestEndpoints> logger)
+    private static IResult Version(ILogger<TestEndpoints> logger)
     {
         var ret = Assembly.GetEntryAssembly()?.GetName().Version?.ToString();
         logger.LogInformation("Version Test {ret}", ret);
-        return TypedResults.Ok(ret);
+        return Results.Text(ret ?? string.Empty, "text/plain");
     }
 
     // GET api/v1/test/getappsettingsversion
     //[HttpGet(TestApiRoutes.Test.GetAppSettingsVersion)]
-    private static Ok<string> AppSettingsVersion(ILogger<TestEndpoints> logger, IConfiguration config)
+    private static IResult AppSettingsVersion(ILogger<TestEndpoints> logger, IConfiguration config)
     {
         var versionInfo = VersionInfo.Create(config);
         var ret = versionInfo is null ? "Version not detected" : versionInfo.AppSettingsVersion;
         logger.LogInformation("Version Test {ret}", ret);
-        return TypedResults.Ok(ret);
+        return Results.Text(ret ?? string.Empty, "text/plain");
     }
 
     // GET api/v1/test/getsettings
     //[HttpGet(TestApiRoutes.Test.GetSettings)]
-    private static Ok<string> Settings()
+    private static IResult Settings()
     {
         var sb = new StringBuilder();
         sb.AppendLine($"Host name: {Environment.MachineName.Capitalize()}");
@@ -103,6 +103,6 @@ public sealed class TestEndpoints : IInstaller
         //    sb.AppendLine("View Api Keys Finished");
         //}
 
-        return TypedResults.Ok(sb.ToString());
+        return Results.Text(sb.ToString(), "text/plain");
     }
 }
