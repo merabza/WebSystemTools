318ac655915d5b1d224dc4c0db54b72c7a6f611f
merabza <merabza@gmail.com>
2025-12-31 17:03:09 +0400

    202512311702

---
+++ MediatorTools/DependencyInjection/MediatorDependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MediatorTools.DependencyInjection;
+
+// ReSharper disable once UnusedType.Global
+public static class MediatorDependencyInjection
+{
+    public static IServiceCollection AddMediator(this IServiceCollection services, IConfiguration configuration,
+        bool debugMode, params Assembly[] assemblies)
+    {
+        if (debugMode)
+            Console.WriteLine($"{nameof(AddMediator)} Started");
+
+        var mediatRSettings = configuration.GetSection("MediatRLicenseKey");
+
+        var mediatRLicenseKey = mediatRSettings.Value;
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.LicenseKey = mediatRLicenseKey;
+
+            foreach (var assembly in assemblies) cfg.RegisterServicesFromAssembly(assembly);
+        });
+
+        if (debugMode)
+            Console.WriteLine($"{nameof(AddMediator)} Finished ");
+
+        return services;
+    }
+}
