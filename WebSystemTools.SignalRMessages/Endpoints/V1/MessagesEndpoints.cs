c1322aa4bd31bf18442ba4bdb69a28b55442525a
merabza <merabza@gmail.com>
2026-06-07 18:25:38 +0400

    202606071825

--- WebSystemTools.SignalRMessages/Endpoints/V1/MessagesEndpoints.cs
+++ WebSystemTools.SignalRMessages/Endpoints/V1/MessagesEndpoints.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.AspNetCore.Routing;
-using Serilog;
 using SystemTools.StringMessagesApiContracts.V1.Routes;
 
 namespace WebSystemTools.SignalRMessages.Endpoints.V1;
@@ -9,14 +9,20 @@ namespace WebSystemTools.SignalRMessages.Endpoints.V1;
 // ReSharper disable once UnusedType.Global
 public static class MessagesEndpoints
 {
-    public static bool UseSignalRMessagesHub(this IEndpointRouteBuilder endpoints, ILogger? debugLogger)
+    public static bool UseSignalRMessagesHub(this IEndpointRouteBuilder endpoints, bool debugMode)
     {
-        debugLogger?.Information("{MethodName} Started", nameof(UseSignalRMessagesHub));
+        if (debugMode)
+        {
+            Console.WriteLine($"{nameof(UseSignalRMessagesHub)} Started");
+        }
 
         endpoints.MapHub<MessagesHub>(MessagesRoutes.ApiBase + MessagesRoutes.Messages.MessagesRoute,
             options => { options.Transports = HttpTransportType.LongPolling; }).RequireAuthorization();
 
-        debugLogger?.Information("{MethodName} Finished", nameof(UseSignalRMessagesHub));
+        if (debugMode)
+        {
+            Console.WriteLine($"{nameof(UseSignalRMessagesHub)} Finished");
+        }
 
         return true;
     }
